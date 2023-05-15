19851166d7e1e10536d58499627d668658578067
afrazier01 <amfrazier01@gmail.com>
2023-05-14 22:35:15 -0500

    added question file and and added nodemon debugger

--- index.js
+++ index.js
@@ -1,73 +1,20 @@
 // TODO: Include packages needed for this application
+const {questions} = require('./questions')
 const inquirer = require('inquirer')
+const fs = require('fs')
+
 // TODO: Create an array of questions for user input
-const questions = [
-    'What is the name of your project?',
-    'What is should your description read?',
-    'Are there installation instructions?',
-    'What should be included in the usage section?',
-    'What are the contribution guidelines?'
-];
-
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What iis your GitHub username?',
-            name: 'github',
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is the name of your project?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'Please write a short description of your project',
-            name: 'description',
-        },
-        {
-            type: 'checkbox',
-            message: 'What kind of license should your project have?',
-            name: 'license',
-            choices: ['MIT','APACHE 2.0','GPL 3.0','BSD 3','None']
-        },
-        {
-            type: 'input',
-            message: 'What command should be run to install dependencies? ',
-            name: 'dependencies',
-            default: 'npm i'
-        },
-        {
-            type: 'input',
-            message: 'What command should be run to run tests?',
-            name: 'test',
-            default: 'npm test'
-        },
-        {
-            type: 'input',
-            message: 'What does the user need to know about using the repo?',
-            name: 'information',
-        },
-        {
-            type: 'input',
-            message: 'What does the user need to know about contributing to the repo?',
-            name: 'contribution',
-        }
-    ])
-    .then( (data) => {
+function askQuestions() {
+    inquirer.prompt(questions).then((data) => {
         console.log(data)
-        const fileName = `${data.title}`
 
-        function writeToFile(fileName, data) {}
-    }
-    )
-    .catch((err) => {console.log(err)});
+        const {github, email, title, description, license, dependencies, test, information, contribution} = data
+        console.log('contribution >>'+contribution)
+        console.log('title >>'+title)
+        // fs.appendFile()
+        //appendFile
+    }).catch((err) => console.log(err));
+} 
 // TODO: Create a function to write README file
 
 
@@ -76,3 +23,4 @@ function init() {}
 
 // Function call to initialize app
 init();
+askQuestions();
